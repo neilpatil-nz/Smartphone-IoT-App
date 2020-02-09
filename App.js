95f5d06d45bbffe3bd7eb93784655530aafaf55e
neilpatil-nz <57848464+neilpatil-nz@users.noreply.github.com>
2020-02-09 20:57:04 +1300

    1.1

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Autodrive" onPress = {() => setOutputText('The text has changed')}/>
+    <View style={{padding: 100}}>
+      <View>
+        <TextInput placeholder = "Enter IP Address"
+        style ={{ 
+        borderColor:'grey', borderWidth:0.1, 
+        borderRadius:4, height:40, fontSize: 20, padding: 5}}
+        
+        />
+        <Button title='CONNECT'/>
+      </View>
+      <Text></Text>
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
