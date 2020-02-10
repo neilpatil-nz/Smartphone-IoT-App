1cfbccf188f1ffa899246a1933eb9ca2e846ed6b
neilpatil-nz <57848464+neilpatil-nz@users.noreply.github.com>
2020-02-11 12:30:32 +1300

    1.3
    Fully functioning controls. Improvements
    needed in URL fetching and UI

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Button, TextInput, Linking, TouchableWithoutFee
 export default function App() {
   const [enteredURL, setURL] = useState(''); 
   const mainURL = ('');
-  const commandUP = ('/w');
+  const commandUP = ('/forward');
   const commandLEFT = ('/left');
   const commandBACK = ('/reverse');
   const commandRIGHT = ('/right');
@@ -50,10 +50,11 @@ export default function App() {
       method: 'GET'
     });   
   };
+ 
 
   return (
+   
     <View style={{padding: 100}}>
-      
       <View>
         <TextInput placeholder = "Enter IP Address"
         style ={{ 
@@ -71,41 +72,108 @@ export default function App() {
         onPressIn={moveUP}
         onPressOut = {moveSTOP}
         >
-        <View style = {{padding: 30, margin: 20, backgroundColor: 'purple', alignItems: 'center'}}>
-           <Text style = {{color: 'white'}}>FORWARD</Text>
-        </View>
+         <View style={[styles.triangle]} />
         </TouchableWithoutFeedback>
       </View>
       <View>
         <TouchableWithoutFeedback 
         onPressIn= {moveLEFT}
         onPressOut = {moveSTOP}>
-          <View style = {{width: 300, height: 50, margin: 20, backgroundColor: 'purple', alignItems: 'center'}}>
-           <Text>LEFT</Text>
-          </View> 
+          <View style={[styles.triangleLeft]} />
         </TouchableWithoutFeedback>
       </View>
       <View>
         <TouchableWithoutFeedback 
         onPressIn= {moveRIGHT}
         onPressOut = {moveSTOP}>
-          <View style = {{width: 300, height: 50, margin: 20, backgroundColor: 'purple', alignItems: 'center'}}>
-           <Text>RIGHT</Text>
-          </View> 
+          <View style={[styles.triangleRight]} />
         </TouchableWithoutFeedback>
       </View>
       <View>
         <TouchableWithoutFeedback 
         onPressIn= {moveBACK}
         onPressOut = {moveSTOP}>
-          <View style = {{width: 300, height: 50, margin: 20, backgroundColor: 'purple', alignItems: 'center'}}>
-           <Text>REVERSE</Text>
-          </View> 
+          <View style={[styles.triangleDown]} />
         </TouchableWithoutFeedback>
       </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  triangle: {
+    position: 'absolute',
+    width: 0,
+    height: 0,
+    marginTop: 60,
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftWidth: 30,
+    borderRightWidth: 30,
+    borderBottomWidth: 60,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'black'
+  },
+  triangleLeft: {
+    position: 'absolute',
+    
+    marginTop: 130,
+    right: 150,
+    width: 0,
+    height: 0,
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftWidth: 30,
+    borderRightWidth: 30,
+    borderBottomWidth: 60,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'black',
+    transform: [
+      {rotate: '-90deg'}
+    ],
+  },
+  triangleRight: {
+    position: 'absolute',
+    width: 0,
+    marginTop: 130,
+    left: 150,
+    height: 0,
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftWidth: 30,
+    borderRightWidth: 30,
+    borderBottomWidth: 60,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'black',
+    transform: [
+      {rotate: '90deg'}
+    ],
+  },
+  triangleDown: {
+    position: 'absolute',
+    width: 0,
+    marginTop: 200,
+    
+    height: 0,
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftWidth: 30,
+    borderRightWidth: 30,
+    borderBottomWidth: 60,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#cfd8dc',
+    transform: [
+      {rotate: '180deg'}
+    ],
+  },
+});
 
 /*const styles = StyleSheet.create({
   container: {
